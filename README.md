58b580c48d9af0d25a6b38f6b8923651a2b94112
NAKIVO, Inc <github@nakivo.com>
2019-04-10 12:29:24 +0300

    NAKIVO Backup & Replication 8.5 (Beta)

---
+++ README.md
@@ -0,0 +1,9 @@
+# NAKIVO Backup & Replication
+
+NAKIVO Backup and Replication is a fast, reliable, and affordable solution for protecting VMware vSphere, Microsoft Hyper-V and AWS EC2 environments. NAKIVO Backup and Replication offers great backup performance, reliability, and advanced recovery features.
+
+## Support
+
+```
+support@nakivo.com
+```
---
+++ post_install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+PRODUCT='NAKIVO Backup & Replication'
+URL="https://d96i82q710b04.cloudfront.net/res/product/beta/NAKIVO_Backup_Replication_v8.5.0_Installer-BETA.sh"
+SHA256="fb644470904580904c05e267218380d4903fee5e13297f7aa879f8a2dbdacc6d"
+
+PRODUCT_ROOT="/usr/local/nakivo"
+INSTALL="inst.sh"
+
+curl --fail --tlsv1.2 -o $INSTALL $URL
+if [ $? -ne 0 -o ! -e $INSTALL ]; then
+    echo "ERROR: Failed to get $PRODUCT installer"
+    rm $INSTALL >/dev/null 2>&1
+    exit 1
+fi
+
+CHECKSUM=`sha256 -q $INSTALL`
+if [ "$SHA256" != "$CHECKSUM" ]; then
+    echo "ERROR: Incorrect $PRODUCT installer checksum"
+    rm $INSTALL >/dev/null 2>&1
+    exit 2
+fi
+
+sh ./$INSTALL -f -y -i "$PRODUCT_ROOT" --eula-accept --extract 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: $PRODUCT install failed"
+    rm $INSTALL >/dev/null 2>&1
+    exit 3
+fi
+rm $INSTALL >/dev/null 2>&1
+
+#disable default HTTP ports redirect
+SVC_PATH="$PRODUCT_ROOT/director"
+awk 'BEGIN{A=0} /port="80/{A=1} {if (A==0) print $0} />/{A=0}' $SVC_PATH/tomcat/conf/server-linux.xml >$SVC_PATH/tomcat/conf/server-linux.xml_ 2>/dev/null
+mv $SVC_PATH/tomcat/conf/server-linux.xml_ $SVC_PATH/tomcat/conf/server-linux.xml >/dev/null 2>&1
+
+#enforce EULA
+PROFILE=`ls "$SVC_PATH/userdata/"*.profile 2>/dev/null | head -1`
+if [ "x$PROFILE" != "x" ]; then
+    sed -e 's@"system.licensing.eula.must.agree": false@"system.licensing.eula.must.agree": true@' "$PROFILE" >"${PROFILE}_" 2>/dev/null
+    mv "${PROFILE}_" "$PROFILE" >/dev/null 2>&1
+fi
+
+service nkv_dirsvc start >/dev/null 2>&1
